964326d4a921b807ea33b65d6b52b243ad3fa733
Chase Thompson <chase.w.thompson210@gmail.com>
2020-03-17 22:17:52 -0500

    New coding challenge solution

--- CodeWars.py
+++ CodeWars.py
@@ -151,3 +151,14 @@ def likes(names):
 """
 https://www.codewars.com/kata/5266876b8f4bf2da9b000362/solutions/python
 """
+
+"""
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+"""
+
+def duplicate_encode(word):
+    return "".join(["(" if word.lower().count(c) == 1 else ")" for c in word.lower()])
+
+"""
+https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/solutions/python
+"""
